eaa9723d3af12511f01e25b52b63ef7c23a270a2
arbabsiddiqui109-sys <arbabsiddiqui109@gmail.com>
2026-03-17 18:23:41 +0530

    Update LOCKED_GROUP_NAME with new value

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ try {
 
 // ✅ Group Info (change these)
 const GROUP_THREAD_ID = "1514671402956626";
-const LOCKED_GROUP_NAME = "🫸𝗦𝗜𝗧𝗔 𝗣𝗔𝗥𝗪𝗔𝗧𝗜 𝗞𝗔𝗬𝗔𝗠𝗔𝗧 𝗧𝗔𝗞 𝗖𝗛𝗨𝗗𝗘𝗚𝗜 𝗟𝗔𝗚𝗔𝗟𝗘 𝗗𝗨𝗠 𝗟𝗨𝗡𝗗𝗕𝗛𝗔𝗞𝗧 🔵💪🤣"; 
+const LOCKED_GROUP_NAME = "🫸संजय की माँ रण्डी बन गयी रोनी का लोडा चूस कर 🔵💪🤣"; 
   // Locked nameLockedxpress Server to keep bot alive (for Render or UptimeRobot)
 const app = express();
 const PORT = process.env.PORT || 3000;
