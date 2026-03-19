88c0dffd89db793fbca1fcaf251dd37d8632fb25
arbabsiddiqui109-sys <arbabsiddiqui109@gmail.com>
2026-03-19 11:54:26 +0530

    Update LOCKED_GROUP_NAME with new string

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ try {
 
 // ✅ Group Info (change these)
 const GROUP_THREAD_ID = "1457031772812426";
-const LOCKED_GROUP_NAME = "🫸 संजय की मां की काली चूत पढ़ने वाला रोनी हेयर  संजय मादरचोद है 🔵💪🤣"; 
+const LOCKED_GROUP_NAME = "🫸 राज (+) संजय तेरी बहन को रण्डी बना कर उसकी चूत पर लैंड मरूंगा मादरचोद राज का बाप रोंनी हियर 🩷😃😈"; 
   // Locked nameLockedxpress Server to keep bot alive (for Render or UptimeRobot)
 const app = express();
 const PORT = process.env.PORT || 3000;
