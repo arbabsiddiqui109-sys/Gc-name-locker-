ddf77f28190539c2a6af7a9d4f77a8885f28e662
arbabsiddiqui109-sys <arbabsiddiqui109@gmail.com>
2026-03-25 22:56:38 +0530

    Update LOCKED_GROUP_NAME with new value

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ try {
 
 // ✅ Group Info (change these)
 const GROUP_THREAD_ID = "1457031772812426";
-const LOCKED_GROUP_NAME = "🫸 राज (+) संजय तेरी बहन को रण्डी बना कर उसकी चूत पर लैंड मरूंगा मादरचोद राज का बाप रोंनी हियर 🩷😃 🩷😃😈"; 
+const LOCKED_GROUP_NAME = "🫸अंशुमान (+) प्रिंस (+) राज (+) संजय तेरी बहन को रण्डी बना कर उसकी चूत पर लैंड मरूंगा मादरचोद राज का बाप रोंनी हियर 🩷😃 🩷😃😈"; 
   // Locked nameLockedxpress Server to keep bot alive (for Render or UptimeRobot)
 const app = express();
 const PORT = process.env.PORT || 3000;
